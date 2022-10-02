8129f126d6ce4fdc07ff565ba910a8071ef9b7f9
Arup Parida <arupggn@gmail.com>
2022-10-02 21:28:48 +0530

    Add files via upload

---
+++ Day12_delegate/multithreadingdemo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Day12_delegate
+{
+    class multithreadingdemo
+    {
+        void Runthread_Task()
+        {
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("\tMsg #{0} from the secondary thread", i);
+            }
+        }
+        void RunThread()
+        {
+            Thread t = new Thread(new ThreadStart(Runthread_Task));
+            //run the new thread
+            t.Start();
+            //print some mesaage to the console window
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("Msg #{0} from Main Thread", i);
+            }
+            Console.WriteLine("End of Main Thread Program");
+        }
+        public static void Main()
+        {
+            multithreadingdemo obj = new multithreadingdemo();
+            obj.RunThread();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day12_delegate/singlecastdelegate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day12_delegate
+{
+    delegate void DelegateString(string s);
+    class singlecastdelegate
+    {
+        public static void Greet(string Name)
+        {
+            Console.WriteLine("Hello...." + Name);
+        }
+        public static void Main()
+        {
+            //instantiate delegate
+            DelegateString delegateobj1 = new DelegateString(Greet);
+            //invoke delegate
+            delegateobj1("goodmorning");
+            Console.WriteLine("----------------------");
+            Console.ReadLine();
+        }
+    }
+}

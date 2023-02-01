b3da6e567d0402b51f5d8838bb480f9da891d5c9
Arup Parida <arupggn@gmail.com>
2023-02-01 23:52:42 +0530

    Update Program.cs

--- Day11_collections/Program.cs
+++ Day11_collections/Program.cs
@@ -10,6 +10,22 @@ namespace Day11_collections
     {
         static void Main(string[] args)
         {
+            
+            //creating a list of integers
+            List<int> New_List = new List<int>();
+            //<int> Nlist = new <int>
+            //adding items to list
+            for(int i = 0; i<10;i++)
+            {
+                New_List.Add(i * 3);                
+            }         
+            //display list items
+            foreach (var items in New_List)
+            {
+                Console.WriteLine(items);
+            }
+                
+            
         }
     }
 }

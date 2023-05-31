ff42c5938b00c9fcfb91a53e11a713aad31af59c
Arup Parida <arupggn@gmail.com>
2023-05-31 13:15:41 +0530

    inheritance examples added

---
+++ Cls_Inheritance1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCsharpPlayground
+{
+    class Cls_Inheritance1
+    {
+
+        //Practicing inheritance and types of Inheritance 
+        /*
+         * Inheritance : It is carrying forward the properties or traits of a class to another class
+         * Code reuseability is the key focus during inheritance 
+         * Using Inheritance one or more classes can be derived from an existing class
+         * The existing class is called parent/base class and the inherited class is derived/inherited class
+         * Types of inheritance 
+         * 1. single 
+         * 2. hierarchical
+         * 3. Multi-level
+         * 4. Multiple(using Interface)
+         * A Quick go through can be done from  : https://www.c-sharpcorner.com/UploadFile/0c1bb2/types-of-inheritance-in-C-Sharp/
+         * 
+     
+         */
+
+        public static void Main()
+        {
+            debitinfo nobj = new debitinfo();
+            Console.WriteLine(nobj.Debit());
+            Console.WriteLine(nobj.Credits());
+            //nobj.Debit();
+        }
+    }
+
+        public class AccountCreditInfo //base class
+    { 
+        public string Credits()
+        {
+            return "Balance is credited";
+
+        }
+    }
+
+    public class debitinfo : AccountCreditInfo  //derived class
+    {
+        public string Debit()
+        
+        {
+            Credits();
+            return "Balance is debited ";
+        }
+
+       
+
+
+        
+    }
+}
--- CountOccurence.cs
+++ CountOccurence.cs
@@ -6,7 +6,51 @@ using System.Threading.Tasks;
 
 namespace MyCsharpPlayground
 {
+
+    /*Problem Statement  -  Write a program to count the occurrences of each character*/
+
+    /* There are three ways tod this problem 
+     * 1. Using recursion
+     * 2. Using dictionary 
+     * 3. Using the normal looping method 
+     */
     class CountOccurence
     {
+        public static void Main()
+        {
+
+            string str = "Website";
+            Console.WriteLine("String: " + str);
+            while (str.Length > 0)
+            {
+                Console.Write(str[0] + " = ");
+                int cal = 0;
+                for (int j = 0; j < str.Length; j++)
+                {
+                    if (str[0] == str[j])
+                    {
+                        cal++;
+                    }
+                }
+                Console.WriteLine(cal);
+                str = str.Replace(str[0].ToString(), string.Empty);
+                //Console.WriteLine(str);
+            }
+            Console.ReadLine();
+
+
+
+            /*
+            Console.Write("Pls enter a string:");
+            string str = Console.ReadLine();
+            //Convert str to chararray
+            Array[] arr = str.Split();
+            Array[] arr = new Array['A','R','U','P',]
+
+            foreach( chr in arr)
+            {
+
+            }*/
+        }
     }
 }
